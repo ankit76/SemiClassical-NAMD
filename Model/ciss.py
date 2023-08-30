e4f08478929b99bf958d505ac0545a17791250d5
Ankit Mahajan <ankitmahajan76@gmail.com>
2023-08-29 22:35:35 -0400

    faster ciss

--- Model/ciss.py
+++ Model/ciss.py
@@ -1,80 +1,111 @@
+import os
 import numpy as np
-from numpy import array as A
+os.environ['JAX_PLATFORM_NAME'] = 'cpu'
+#os.environ['JAX_ENABLE_X64'] = 'True'
+from jax import jit, vmap, lax, numpy as jnp
+from flax import struct
+from typing import Callable, Any
+from dataclasses import dataclass
+from functools import partial
 
 
+@struct.dataclass
 class parameters():
-   dtN = 100.0
-   time = 1000
-   ps = 41342
-   NSteps = time * ps // int(dtN)  # int(2*10**6)
-   NTraj = 1
-   dtE = dtN/20
-   NStates = 5
-   M = 1
-   initState = 0
-   nskip = 10
-   beta = 1 / 0.00095
-   theta = np.pi / 16
-   gamma_1 = 0.00001837465
-   gamma_2 = gamma_1 / 2
-   j_exc = 4.2629194e-6
-   ev = 27.211385
-   bath = np.loadtxt('bath.txt')
-   omega, c_1 = bath[:, 0], bath[:, 1]
-   lambda_1 = 0.1
-   lambda_2 = 0.2
-   e_ct1 = -0.1 / ev + lambda_1 / ev
-   e_ct2 = -0.35 / ev + (np.sqrt(lambda_1 / ev) + np.sqrt(lambda_2 / ev))**2
-   c_2 = (np.sqrt(lambda_2) + np.sqrt(lambda_1)) * c_1 / np.sqrt(lambda_1)
-   ndof = len(c_1)
+    dtN: float = 100.0
+    dtE: float = dtN / 20
+    time: int = 1000
+    ps: int = 41342
+    NSteps: int = int(time * ps / dtN)  # int(2*10**6)
+    NESteps: int = int(dtN / dtE) // 2
+    dtE: float = 2 * dtN / NESteps
+    NTraj: int = 10
+    NStates: int = 5
+    M: int = 1
+    initState: int = 0
+    nskip: int = 10
+    beta: float = 1 / 0.00095
+    theta: float = jnp.pi / 16
+    gamma_1: float = 0.00001837465
+    j_exc: float = 4.2629194e-6
+    ev: float = 27.211385
+    gamma_2: float = gamma_1 / 2
+    bath: Any = jnp.array(np.loadtxt('bath.txt'))
+    omega: Any = bath[:, 0]
+    c_1: Any = bath[:, 1]
+    lambda_1: float = 0.1
+    lambda_2: float = 0.2
+    e_ct1: float = -0.1 / ev + lambda_1 / ev
+    e_ct2: float = -0.35 / ev + \
+        (np.sqrt(lambda_1 / ev) + np.sqrt(lambda_2 / ev))**2
+    c_2: float = (np.sqrt(lambda_2) + np.sqrt(lambda_1)) * \
+        c_1 / np.sqrt(lambda_1)
+    ndof: int = c_1.size
 
-def Hel(R):
+    #@partial(jit, static_argnums=(0,))
+    @jit
+    def Hel(self, R):
+        Vij = jnp.zeros((5, 5)) + 0.j
+    
+        Vij = Vij.at[1, 1].set(self.e_ct1 + self.j_exc + jnp.sum(self.c_1 * R))
+        Vij = Vij.at[2, 2].set(self.e_ct1 - self.j_exc + jnp.sum(self.c_1 * R))
+        Vij = Vij.at[3, 3].set(self.e_ct2 + self.j_exc + jnp.sum(self.c_2 * R))
+        Vij = Vij.at[4, 4].set(self.e_ct2 + self.j_exc + jnp.sum(self.c_2 * R))
+
+        Vij = Vij.at[0, 1].set(self.gamma_1 * jnp.cos(self.theta))
+        Vij = Vij.at[1, 0].set(self.gamma_1 * jnp.cos(self.theta))
+        Vij = Vij.at[0, 2].set(-1.j * self.gamma_1 * jnp.sin(self.theta))
+        Vij = Vij.at[2, 0].set(1.j * self.gamma_1 * jnp.sin(self.theta))
+        Vij = Vij.at[1, 3].set(self.gamma_2)
+        Vij = Vij.at[3, 1].set(self.gamma_2)
+        Vij = Vij.at[2, 4].set(self.gamma_2)
+        Vij = Vij.at[4, 2].set(self.gamma_2)
+        return Vij
+
+    #@partial(jit, static_argnums=(0,))
+    @jit
+    def dHel0(self, R):
+        omega = self.omega
+        dH0 = omega**2 * R
+        return dH0
+
+    #@partial(jit, static_argnums=(0,))
+    @jit
+    def dHel(self, R):
+        c_1 = self.c_1
+        c_2 = self.c_2
+    
+        dHij = jnp.zeros((5, 5, R.size))
+        dHij = dHij.at[1, 1, :].set(c_1)
+        dHij = dHij.at[2, 2, :].set(c_1)
+        dHij = dHij.at[3, 3, :].set(c_2)
+        dHij = dHij.at[4, 4, :].set(c_2)
+        return dHij
+
+    #@partial(jit, static_argnums=(0,))
+    def initR(self):
+        R0 = 0.0
+        P0 = 0.0
+        beta = self.beta
+        omega = self.omega
+        ndof = self.ndof
+
+        sigP = jnp.sqrt(omega / (2 * jnp.tanh(0.5*beta*omega)))
+        sigR = sigP/omega
+
+        R = jnp.zeros((ndof))
+        P = jnp.zeros((ndof))
+        for d in range(ndof):
+            R = R.at[d].set(np.random.normal()*sigR[d])
+            P = P.at[d].set(np.random.normal()*sigP[d])
+        return R, P
+
+    def __hash__(self):
+        return hash((self.dtN, self.time, self.ps, self.NTraj, self.NStates, self.M, self.initState, self.nskip, self.beta, self.theta, self.gamma_1, self.j_exc, self.ev, self.NSteps, self.dtE, self.gamma_2, self.bath, self.omega, self.c_1, self.lambda_1, self.lambda_2, self.e_ct1, self.e_ct2, self.c_2, self.ndof))
+
+
+if __name__ == '__main__':
     par = parameters()
-    Vij = np.zeros((5, 5), dtype=complex)
-
-    Vij[1, 1] = par.e_ct1 + par.j_exc + np.sum(par.c_1 * R)
-    Vij[2, 2] = par.e_ct1 - par.j_exc + np.sum(par.c_1 * R)
-    Vij[3, 3] = par.e_ct2 + par.j_exc + np.sum(par.c_2 * R)
-    Vij[4, 4] = par.e_ct2 + par.j_exc + np.sum(par.c_2 * R)
-
-    Vij[0, 1], Vij[1, 0] = par.gamma_1 * np.cos(par.theta), par.gamma_1 * np.cos(par.theta)
-    Vij[0, 2], Vij[2, 0] = -1.j * par.gamma_1 * np.sin(par.theta), 1.j * par.gamma_1 * np.sin(par.theta)
-    Vij[1, 3], Vij[3, 1] = par.gamma_2, par.gamma_2
-    Vij[2, 4], Vij[4, 2] = par.gamma_2, par.gamma_2
-    return Vij
-
-
-def dHel0(R):
-    omega = parameters.omega
-    dH0 = omega**2 * R
-    return dH0
-
-
-def dHel(R):
-    c_1 = parameters.c_1
-    c_2 = parameters.c_2
-
-    dHij = np.zeros((5, 5, len(R)))
-    dHij[1, 1, :] = c_1
-    dHij[2, 2, :] = c_1
-    dHij[3, 3, :] = c_2
-    dHij[4, 4, :] = c_2
-    return dHij
-
-
-def initR():
-    R0 = 0.0
-    P0 = 0.0
-    beta = parameters.beta
-    omega = parameters.omega
-    ndof = parameters.ndof
-
-    sigP = np.sqrt(omega / (2 * np.tanh(0.5*beta*omega)))
-    sigR = sigP/omega
-
-    R = np.zeros((ndof))
-    P = np.zeros((ndof))
-    for d in range(ndof):
-        R[d] = np.random.normal()*sigR[d]
-        P[d] = np.random.normal()*sigP[d]
-    return R, P
+    R, P = par.initR()
+    print(R.shape)
+    print(P.shape)
+    print(par.dHel(R).shape)
